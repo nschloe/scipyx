6626f075bd751e28697938cb7f4b90dc0891f078
Nico Schlömer <nico.schloemer@gmail.com>
2021-09-02 15:12:01 +0200

    remove __about__

--- src/scipyx/__init__.py
+++ src/scipyx/__init__.py
@@ -1,11 +1,9 @@
-from .__about__ import __version__
 from ._interpolation import interp_rolling_lagrange
 from ._krylov import bicg, bicgstab, cg, cgs, gmres, minres, qmr
 from ._nonlinear import bisect, regula_falsi
 from ._optimize import leastsq, minimize
 
 __all__ = [
-    "__version__",
     "bicg",
     "bicgstab",
     "cg",
